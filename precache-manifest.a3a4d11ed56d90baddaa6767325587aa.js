1fdf4aec2402de5fbf009557b21ed9988948795c
LitoMore <litomore@gmail.com>
2020-02-28 06:03:48 +0800

    Updates

---
+++ precache-manifest.a3a4d11ed56d90baddaa6767325587aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2afe25843d299c1b7bc06ac1850cb81",
+    "url": "/fanfou-suicide/index.html"
+  },
+  {
+    "revision": "5374b026019506d2366c",
+    "url": "/fanfou-suicide/static/css/2.bc82e399.chunk.css"
+  },
+  {
+    "revision": "d6e6692530847cf493b8",
+    "url": "/fanfou-suicide/static/css/main.6afccd19.chunk.css"
+  },
+  {
+    "revision": "5374b026019506d2366c",
+    "url": "/fanfou-suicide/static/js/2.cf56cc2f.chunk.js"
+  },
+  {
+    "revision": "e20cf8e41ddefd1b1413212500d781b5",
+    "url": "/fanfou-suicide/static/js/2.cf56cc2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6e6692530847cf493b8",
+    "url": "/fanfou-suicide/static/js/main.1c10f52a.chunk.js"
+  },
+  {
+    "revision": "d060f866af65c8e41d66",
+    "url": "/fanfou-suicide/static/js/runtime-main.d6b84207.js"
+  },
+  {
+    "revision": "783bb457ed9b3294fe72f4b4bc9358a9",
+    "url": "/fanfou-suicide/static/media/Zpix.783bb457.ttf"
+  }
+]);
